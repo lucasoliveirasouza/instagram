28854cd6a3097591afc9e5a47b48919e2294338b
lucasoliveirasouza <>
2022-04-30 15:29:59 -0300

    criar tela parcial de login #1

--- lib/views/auth/login.dart
+++ lib/views/auth/login.dart
@@ -12,6 +12,49 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 5,
+            ),
+            Center(
+              child: Text("português (Brasil)"),
+            ),
+            SizedBox(
+              height: 180,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 30,right: 30),
+              child: TextFormField(
+
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 30,right: 30),
+              child: TextFormField(
+
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.only(left: 30,right: 30),
+              child: ElevatedButton(
+                onPressed: (){},
+                child: Text("Entrar"),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
