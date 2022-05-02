1faac1ca48bcb893205cd6c2aacdecd1e10f1c32
lucassouzapb <lycasoliveira@gmail.com>
2022-05-02 14:38:31 -0300

    Arrumar icones do menu #7

--- lib/views/feed/feed.dart
+++ lib/views/feed/feed.dart
@@ -10,6 +10,42 @@ class FeedView extends StatefulWidget {
 class _FeedViewState extends State<FeedView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Image.asset(
+          "assets/images/instagram.png",
+          height: 30,
+        ),
+        actions: [
+          Icon(
+            Icons.add_box_outlined,
+            color: Colors.black,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Icon(
+            Icons.favorite_border,
+            color: Colors.black,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Icon(
+            Icons.messenger_outline,
+            color: Colors.black,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.white,
+      ),
+    );
   }
 }
